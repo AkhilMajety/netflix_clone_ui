d313a6eac5ef97587f954744c81da1eb0e7420c5
Akhil <babloo5300@gmail.com>
2021-01-12 12:26:55 +0530

    first commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_clone_ui/widgets/widgets.dart';
+import 'package:netflix_clone_ui/data/data.dart';
+
+class HomeScreen extends StatefulWidget {
+
+
+    const HomeScreen({Key key }) : super(key: key);
+
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+    ScrollController _scrollController;
+  double _scrollOffset = 0.0;
+
+
+  @override
+  void initState() {
+    _scrollController = ScrollController()
+      ..addListener(() {
+        setState(() {
+          _scrollOffset = _scrollController.offset;
+        });
+      });
+    super.initState();
+  }
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+    var _scrollOffset;
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.grey[850],
+        child: const Icon(Icons.cast),
+        onPressed: () => print('cast'),
+      ),
+      appBar: PreferredSize(
+        preferredSize: Size(screenSize.width, 50.0),
+        child: CustomAppBar(scrollOffset: _scrollOffset),
+      ),
+      body: CustomScrollView(
+        controller: _scrollController,
+        slivers: [ 
+          
+          SliverToBoxAdapter( 
+            child: ContentHeader(featuredContent:   sintelContent),
+            ),
+          SliverPadding( 
+            padding: const EdgeInsets.only(top: 20),
+            sliver: SliverToBoxAdapter( 
+              child: Previews(  
+                key: PageStorageKey('previews'),
+                title: 'Previews',
+                contentList: previews,  
+              )
+            ),
+          ),
+          SliverToBoxAdapter( 
+            child: ContentList( 
+               key: PageStorageKey('mylist'),
+              title: 'My List',
+              contentList: myList,
+            )
+          ),
+                SliverToBoxAdapter( 
+            child: ContentList( 
+               key: PageStorageKey('originals'),
+              title: 'Netflix Originals',
+              contentList: originals,
+              isOriginals: true,
+            )
+          ),
+                SliverPadding(
+                  padding: const EdgeInsets.only(bottom: 20),
+
+                  sliver: SliverToBoxAdapter( 
+            child: ContentList( 
+               key: PageStorageKey('trending'),
+              title: 'Trending',
+              contentList: trending,
+            )
+          ),
+                ),
+
+
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+//sample code
+
+
+
+
+// class HomeScreen extends StatefulWidget {
+
+
+  
+
+
+//   @override
+//   _HomeScreenState createState() => _HomeScreenState();
+// }
+
+// class _HomeScreenState extends State<HomeScreen> {
+//     ScrollController _scrollController;
+//   double _scrollOffset = 0.0;
+
+
+//   @override
+//   void initState() {
+//     _scrollController = ScrollController()
+//       ..addListener(() {
+//         setState(() {
+//           _scrollOffset = _scrollController.offset;
+//         });
+//       });
+//     super.initState();
+//   }
+//   @override
+//   void dispose() {
+//     _scrollController.dispose();
+//     super.dispose();
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     final Size screenSize = MediaQuery.of(context).size;
+//     var _scrollOffset;
+//     return Scaffold(
+//       floatingActionButton: FloatingActionButton(
+//         backgroundColor: Colors.grey[850],
+//         child: const Icon(Icons.cast),
+//         onPressed: () => print('cast'),
+//       ),
+//       appBar: PreferredSize(
+//         preferredSize: Size(screenSize.width, 50.0),
+//         child: CustomAppBar(scrollOffset: _scrollOffset),
+//       ),
+//       body: CustomScrollView(
+//         controller: _scrollController,
+//         slivers: [ 
+          
+//           SliverToBoxAdapter( 
+//             child: ContentHeader(featuredContent:   sintelContent),
+//             ),
+          
+//         ],
+//       ),
+//     );
+//   }
+// }
